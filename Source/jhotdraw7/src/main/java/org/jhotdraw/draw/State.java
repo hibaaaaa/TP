820e1b5a0b67fc8be30c91e4c4e0bfe2b801b514
Hiba <hiba.bagane@gmail.com>
2019-04-09 14:09:17 -0400

    Q1 et Q2: Ajouter une façade au modèle + couche d'enrobage des actions

---
+++ Source/jhotdraw7/src/main/java/org/jhotdraw/draw/State.java
@@ -0,0 +1,7 @@
+package org.jhotdraw.draw;
+
+/*
+ * Translates the model property values into a State Representation
+ * Computes the control state and trigger the next-action predicate
+ */
+public class State {}
---
+++ Source/jhotdraw7/src/main/java/org/jhotdraw/draw/action/Proposal.java
@@ -0,0 +1,6 @@
+package org.jhotdraw.draw.action;
+
+/*
+ * A proposal contains the values we want the Model to accept
+ */
+public interface Proposal {}
